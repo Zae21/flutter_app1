517bd641dae7a9e01634ef4257280351f5893b35
Zaenal A. Prayoga <prayoga96@gmail.com>
2021-02-11 09:22:24 +0700

    add new section

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main()  => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Home Screen'),
-      centerTitle: true,
-    ),
-    body: Container(
-      child: Text('Body Text'),
-    )
-  )
+  home: Home()
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Image.asset('assets/img2.jpg'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Text('Click'),
+        backgroundColor: Colors.deepPurple,
+      ),
+    );
+  }
+}
